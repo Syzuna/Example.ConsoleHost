994ac594a7a2eeab653b5e3f1e0b87ffe57ca355
Syzuna <syzuna@t-teske.de>
2020-06-02 21:05:30 +0200

    Add files via upload

---
+++ Example.ConsoleHost/Models/Configuration/DiscordOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.ConsoleHost.Models.Configuration
+{
+    public class DiscordOptions
+    {
+        public string[] Ids { get; set; }
+        public string[] Channels { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Example.ConsoleHost/Models/Configuration/TwitchOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Example.ConsoleHost.Models.Configuration
+{
+    public class TwitchOptions
+    {
+        public Credentials Credentials { get; set; }
+    }
+
+    public class Credentials
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
