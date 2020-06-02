cad58b7847c0b9c08f19d6238108d1d788b84bc7
Syzuna <syzuna@t-teske.de>
2020-06-02 21:03:20 +0200

    Add files via upload

---
+++ Example.ConsoleHost/LifetimeMonitoringHostedService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Example.ConsoleHost.Models.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Example.ConsoleHost
+{
+    public class LifetimeMonitoringHostedService : IHostedService
+    {
+        private readonly ILogger<LifetimeMonitoringHostedService> _logger;
+        private readonly DiscordOptions _discordOptions;
+        private readonly TwitchOptions _twitchOptions;
+
+        public LifetimeMonitoringHostedService(ILogger<LifetimeMonitoringHostedService> logger, IOptions<DiscordOptions> discordOptions, IOptions<TwitchOptions> twitchOptions)
+        {
+            _logger = logger;
+            _discordOptions = discordOptions.Value;
+            _twitchOptions = twitchOptions.Value;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Application started");
+            _logger.LogInformation($"{string.Join(',', _discordOptions.Channels)}");
+            _logger.LogInformation($"{_twitchOptions.Credentials.ClientId}");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogWarning("Application shutting down");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Example.ConsoleHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Example.ConsoleHost.Models.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Example.ConsoleHost
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).RunConsoleAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"), false);
+                })
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    services.Configure<DiscordOptions>(hostingContext.Configuration.GetSection("Discord"));
+                    services.Configure<TwitchOptions>(hostingContext.Configuration.GetSection("Twitch"));
+                    services.AddHostedService<LifetimeMonitoringHostedService>();
+                })
+                .UseConsoleLifetime();
+    }
+}
